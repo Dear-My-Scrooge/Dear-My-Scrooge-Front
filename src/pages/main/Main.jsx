ea6ab615516cfcdd79702423e4d713a5eb051cd8
ZZZINU <98758209+ZZZINU@users.noreply.github.com>
2023-11-07 02:19:15 +0900

    :lipstick: Style: 메인 페이지 - 타이틀 생성

--- src/pages/main/Main.jsx
+++ src/pages/main/Main.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import * as S from "./style";
 
+import PastIcon from "../../assets/images/character/past.png";
+import PresentIcon from "../../assets/images/character/present.png";
+import FutureIcon from "../../assets/images/character/future.png";
+import CandleIcon from "../../assets/images/icon/candle.png";
+
 function Main() {
   return (
     <>
-      <>Main 페이지</>
+      <S.MainWrapper>
+        <S.Title>수연's 스크루지</S.Title>
+        <S.SubTitle>78번째 방문자에요 :)</S.SubTitle>
+      </S.MainWrapper>
     </>
   );
 }
--- src/pages/main/style.jsx
+++ src/pages/main/style.jsx
@@ -1,1 +1,32 @@
 import { styled } from "styled-components";
+import MainBackGroundImg from "../../assets/images/background/main.png";
+
+export const MainWrapper = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  background-image: url(${MainBackGroundImg});
+  background-size: cover;
+  background-position: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const Title = styled.div`
+  display: flex;
+  color: #fff;
+  margin-top: 97px;
+  font-size: 45px;
+  font-weight: 400;
+  line-height: normal;
+  letter-spacing: 4.5px;
+`;
+
+export const SubTitle = styled.div`
+  color: #fff;
+  text-align: center;
+  font-size: 20px;
+  font-weight: 400;
+  line-height: 26.3px;
+  letter-spacing: 1px;
+`;
