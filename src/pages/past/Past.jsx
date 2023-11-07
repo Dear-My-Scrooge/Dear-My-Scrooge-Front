8231ac72c90c008e38c95dc6ee79b1aa39ce546b
ZZZINU <98758209+ZZZINU@users.noreply.github.com>
2023-11-07 14:05:56 +0900

    :lipstick: Style: 과거 보내기 페이지 - 배경 생성

--- src/pages/past/Past.jsx
+++ src/pages/past/Past.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import * as S from "./style";
 
 function Past() {
-  return <>Past</>;
+  return (
+    <>
+      <S.PastWrapper></S.PastWrapper>
+    </>
+  );
 }
 
 export default Past;
--- src/pages/past/style.jsx
+++ src/pages/past/style.jsx
@@ -1,1 +1,13 @@
 import { styled } from "styled-components";
+import PastBackGroundImg from "../../assets/images/background/sendPast.png";
+
+export const PastWrapper = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  background-image: url(${PastBackGroundImg});
+  background-size: cover;
+  background-position: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
