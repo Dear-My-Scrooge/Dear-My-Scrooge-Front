b4cd7404dad7d3cfcdc4381b36bba57099fe3c5b
ZZZINU <98758209+ZZZINU@users.noreply.github.com>
2023-11-08 15:47:12 +0900

    :sparkles: Feat: 편지함 페이지 - 헤더 배경 색상 시간별 설정

--- src/pages/mailbox/Mailbox.jsx
+++ src/pages/mailbox/Mailbox.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import * as S from "./style";
+import Head from "../../components/head/Head";
 
 function Mailbox() {
-  return <>Mailbox</>;
+  const [time, setTime] = useState("과거");
+
+  const handleTimeClick = tab => {
+    setTime(tab);
+  };
+
+  const getBackgroundColor = time => {
+    if (time === "과거") return "#F0D890";
+    else if (time === "현재") return "#810006";
+    else if (time === "미래") return "#083A00";
+  };
+
+  return (
+    <>
+      <S.MailboxWrapper>
+        <Head bgcolor={getBackgroundColor(time)} color={"#000"} />
+      </S.MailboxWrapper>
+    </>
+  );
 }
 
 export default Mailbox;
