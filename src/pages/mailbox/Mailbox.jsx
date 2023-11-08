9ca05a3ed30069f5817daa3285c0b7efc471e607
ZZZINU <98758209+ZZZINU@users.noreply.github.com>
2023-11-08 16:15:30 +0900

    :sparkles: Feat: 편지함 페이지 - 탭 생성

--- src/pages/mailbox/Mailbox.jsx
+++ src/pages/mailbox/Mailbox.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import * as S from "./style";
 import Head from "../../components/head/Head";
+import PastBg from "../../assets/images/background/mailboxPast.png";
+import PresentBg from "../../assets/images/background/mailboxPresent.png";
+import FutureBg from "../../assets/images/background/mailboxFuture.png";
 
 function Mailbox() {
   const [time, setTime] = useState("과거");
@@ -20,10 +23,36 @@ function Mailbox() {
     else return "#FFFFFF";
   };
 
+  const setBackgroundImg = time => {
+    if (time === "과거") return PastBg;
+    else if (time === "현재") return PresentBg;
+    else if (time === "미래") return FutureBg;
+  };
+
   return (
     <>
-      <S.MailboxWrapper>
+      <S.MailboxWrapper bgImg={setBackgroundImg(time)}>
         <Head bgcolor={getBackgroundColor(time)} color={getColor(time)} />
+        <S.TimeTabWrapper color={getColor(time)}>
+          <S.TimeTab
+            onClick={() => setTime("과거")}
+            borderbottom={time === "과거" ? "#000" : "none"}
+          >
+            과거
+          </S.TimeTab>
+          <S.TimeTab
+            onClick={() => setTime("현재")}
+            borderbottom={time === "현재" ? "#fff" : "none"}
+          >
+            현재
+          </S.TimeTab>
+          <S.TimeTab
+            onClick={() => setTime("미래")}
+            borderbottom={time === "미래" ? "#fff" : "none"}
+          >
+            미래
+          </S.TimeTab>
+        </S.TimeTabWrapper>
       </S.MailboxWrapper>
     </>
   );
--- src/pages/mailbox/style.jsx
+++ src/pages/mailbox/style.jsx
@@ -1,3 +1,31 @@
 import { styled } from "styled-components";
 
-export const MailboxWrapper = styled.div``;
+export const MailboxWrapper = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  background-image: url(${props => props.bgImg});
+  background-size: cover;
+  background-position: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const TimeTabWrapper = styled.div`
+  margin-top: 20px;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  gap: 55px;
+  color: ${props => props.color};
+`;
+
+export const TimeTab = styled.div`
+  font-size: 25px;
+  font-weight: 400;
+  line-height: normal;
+  letter-spacing: -1.25px;
+  border-bottom: 3px solid ${props => props.borderbottom};
+  padding: 0px 18px 5px 18px;
+  cursor: pointer;
+`;
