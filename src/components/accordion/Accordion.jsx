46d23ad2637bf21b231fd3958607fca3dd5b5ec6
ZZZINU <98758209+ZZZINU@users.noreply.github.com>
2023-11-08 16:43:42 +0900

    :sparkles: Feat: 아코디언 컴포넌트 - 더보기 버튼 생성

--- src/components/accordion/Accordion.jsx
+++ src/components/accordion/Accordion.jsx
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import * as S from "./style";
 import MoreIcon from "../../assets/images/icon/more.png";
 
 function Accordion() {
+  const [show, setShow] = useState(false);
+
+  const handleShowMore = () => {
+    setShow(!show);
+  };
+
   return (
     <>
       <S.AccordionWrapper>
@@ -10,10 +16,14 @@ function Accordion() {
           <S.AccordionQuestion>
             Q. 과거 수연의 첫 수연의 첫 인상은?
           </S.AccordionQuestion>
-          <S.AccordionMoreButton>
-            <S.AccordionMoreButtonImg src={MoreIcon} />
+          <S.AccordionMoreButton onClick={handleShowMore}>
+            <S.AccordionMoreButtonImg src={MoreIcon} show={show} />
           </S.AccordionMoreButton>
         </S.AccordionQuestionWrapper>
+
+        <S.AccordionAnswer className={show ? "show" : "hide"}>
+          gd
+        </S.AccordionAnswer>
       </S.AccordionWrapper>
     </>
   );
--- src/components/accordion/style.jsx
+++ src/components/accordion/style.jsx
@@ -2,6 +2,7 @@ import { styled } from "styled-components";
 
 export const AccordionWrapper = styled.div`
   display: flex;
+  flex-direction: column;
   margin-top: 20px;
 `;
 
@@ -27,4 +28,17 @@ export const AccordionMoreButton = styled.div``;
 
 export const AccordionMoreButtonImg = styled.img`
   width: 13.091px;
+  transform: ${props => (props.show ? "rotate(0deg)" : "rotate(180deg)")};
+  transition: transform 0.3s;
+`;
+
+export const AccordionAnswer = styled.div`
+  max-height: 0; // 처음에 안보이게
+  overflow: hidden;
+  transition: max-height 0.3s ease-in-out; //접는 속도
+
+  &.show {
+    max-height: 100rem;
+    transition: max-height 0.5s ease-in-out; //펼치는 속도
+  }
 `;
