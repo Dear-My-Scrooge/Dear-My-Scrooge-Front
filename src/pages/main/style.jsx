4da22340dd8d08bd8c65196c0f3db76ac9d1f22f
KimJae0 <a30169123@gmail.com>
2023-11-11 11:31:49 +0900

    favicon

--- src/pages/main/style.jsx
+++ src/pages/main/style.jsx
@@ -1,6 +1,8 @@
 import { styled } from "styled-components";
 import MainBackGroundImg from "../../assets/images/background/main.png";
 
+
+
 export const MainWrapper = styled.div`
   width: 100%;
   min-height: 100vh;
