5f622fe7a5b95c8f2cffcb231b2beec310c2388b
ZZZINU <98758209+ZZZINU@users.noreply.github.com>
2023-11-11 12:07:15 +0900

    :sparkles: Feat: 메인 페이지 연결

--- src/components/candle/Candle.jsx
+++ src/components/candle/Candle.jsx
@@ -3,10 +3,15 @@ import { Link } from "react-router-dom";
 import * as S from "./style";
 import CandleIcon from "../../assets/images/icon/candle.png";
 
-function Candle() {
+function Candle(props) {
   return (
     <>
-      <Link to="/about">
+      <Link
+        to="/about"
+        state={{
+          user_id: props.user_id
+        }}
+      >
         <S.Candle src={CandleIcon} alt="촛불 아이콘" />
       </Link>
     </>
--- src/pages/tree/Tree.jsx
+++ src/pages/tree/Tree.jsx
@@ -1,11 +1,15 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import * as S from "./style";
 
 import GhostIcon from "../../assets/images/icon/ghost.png";
 import TreeIcon from "../../assets/images/icon/tree.png";
 
 function Tree() {
+  const navigate = useNavigate();
+  const { state } = useLocation();
+  const user_id = state && state.user_id;
+
   return (
     <>
       <S.MainWrapper>
@@ -14,15 +18,27 @@ function Tree() {
         </Link>
 
         <S.MainContent>
-          <S.Title>디어 마이 스크루지</S.Title>
+          <Link to={`/${user_id}`}>
+            <S.Title>디어 마이 스크루지</S.Title>
+          </Link>
 
           <S.TreeIcon src={TreeIcon} alt="트리 아이콘" />
 
-          <Link to="/login">
+          <Link
+            to="/login"
+            state={{
+              user_id: user_id
+            }}
+          >
             <S.CreatePage>로그인</S.CreatePage>
           </Link>
 
-          <Link to="/signUp">
+          <Link
+            to="/signUp"
+            state={{
+              user_id: user_id
+            }}
+          >
             <S.CreatePage>내 스크루지 페이지 만들기</S.CreatePage>
           </Link>
         </S.MainContent>
